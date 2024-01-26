b0acae8a5ad220c256e67c5fd2ad23b4803aed3d
Eugene Crosser <evgenii.cherkashin@ionos.com>
2024-01-26 11:59:53 +0100

    arrange build process for the defs file
    Signed-off-by: Eugene Crosser <evgenii.cherkashin@ionos.com>

---
+++ netlinklib/__init__.py
@@ -0,0 +1,1 @@
+""" Higher performance netlink library """
---
+++ setup.py
@@ -0,0 +1,37 @@
+from os import getcwd, chdir
+from re import findall
+from setuptools import setup
+from subprocess import call
+
+def pep_version(s: str) -> str:
+    """Take initial numeric part from the string, to comply with PEP-440"""
+    return "0.1"
+    for i in range(0, len(s)):
+        if not s[i] in "0123456789.":
+            break
+    return s[:i].rstrip(".")
+
+with open("debian/changelog", "r") as clog:
+    _, version, _ = findall(
+        r"(?P<src>.*) \((?P<version>.*)\) (?P<suite>.*); .*",
+        clog.readline().strip(),
+    )[0]
+
+print("Building a definitions file")
+curdir = getcwd()
+chdir("mknetlinkdefs")
+result = call(["make"])
+if result:
+    raise RuntimeError(f"make failed with result={result}")
+chdir(curdir)
+
+print(f"configuring package with version {pep_version(version)}")
+setup(
+    name="netlinklib",
+    version=pep_version(version),
+    description="Higher performance netlink library",
+    author="Eugene Crosser",
+    author_email="evgenii.cherkashin@ionos.com",
+    packages=["netlinklib"],
+    tests_require=["black", "pylint", "mypy"],
+)
