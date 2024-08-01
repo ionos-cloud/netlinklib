986be7b6134512e977da8b332a81cc8c07255e8e
Eugene Crosser <evgenii.cherkashin@ionos.com>
2024-08-01 16:00:21 +0200

    TC: implement new_class for htb
    Signed-off-by: Eugene Crosser <evgenii.cherkashin@ionos.com>

--- netlinklib/api_tc.py
+++ netlinklib/api_tc.py
@@ -1,8 +1,10 @@
 """ Netlink dump implementation replacement for pyroute2 """
 
+from array import array
 from collections import defaultdict
 from functools import partial
 from socket import AF_UNSPEC, socket
+from struct import pack
 from typing import (
     Any,
     Callable,
@@ -16,9 +18,11 @@ from typing import (
 )
 from .classes import (
     tcmsg,
+    tc_htb_opt,
     tc_htb_glob,
     tc_fifo_qopt,
     tc_multiq_qopt,
+    tc_ratespec,
 )  # tc_prio_qopt
 
 # pylint: disable=wildcard-import, unused-wildcard-import
@@ -46,6 +50,24 @@ __all__ = (
     "nll_filter_replace",
     "nll_filter_del",
 )
+
+
+MAX_U32 = 2**32 - 1
+HZ = 100  # Should maybe use `os.sysconf(os.sysconf_names['SC_CLK_TCK'])`
+TIME_UNITS_PER_SEC = 1000000
+with open("/proc/net/psched") as ps:
+    t2us, us2t, clock_res, _ = map(lambda s: int(s, 16), ps.read().split())
+if clock_res == 1000000000:
+    t2us = us2t
+clock_factor = clock_res / TIME_UNITS_PER_SEC  # float, expect < 1
+tick_in_usec = t2us / us2t * clock_factor
+
+U32t = (
+    "L"
+    if array("L").itemsize == 4
+    else "I" if array("I").itemsize == 4 else None
+)
+assert U32t is not None, "What integer type has bytesize 4 on this system?!"
 
 
 def _nll_tc_dump(
@@ -113,6 +135,9 @@ def nll_filter_get(
     return newtfilter_parser(msg)
 
 
+# Functions to use in _extra_attrs dictionary:
+
+
 def no_attrs(**kwargs: Any) -> Tuple[Tuple[int, bytes], ...]:
     return ()
 
@@ -162,6 +187,132 @@ def prio_qdisc_attrs(
     )
 
 
+def _get_size_and_cell(burst: Union[int, Tuple[int, int]]) -> Tuple[int, int]:
+    if isinstance(burst, int):
+        return burst, -1
+    bufs, cell = burst
+    clog = -1
+    for i in range(31):
+        if 1 << i == cell:
+            clog = i
+    return bufs, clog
+
+
+def _tc_adjust_size(sz: int, mpu: int, linklayer: int) -> int:
+    # ignore ATM that uses different adjustment
+    return sz if sz >= mpu else mpu
+
+
+def _make_ratespec_tab(
+    cell_log: int = -1,
+    linklayer: int = TC_LINKLAYER_ETHERNET,
+    overhead: int = 0,
+    mtu: int = 0,
+    mpu: int = 0,
+    rate: int = 0,
+) -> Tuple[tc_ratespec, array]:  # type: ignore [type-arg]  # for new mypy
+    if not mtu:
+        mtu = 2047
+    if cell_log < 0:
+        cell_log = 0
+        while (mtu >> cell_log) > 255:
+            cell_log += 1
+    rtab = array(
+        U32t,  # type: ignore [arg-type]
+        (
+            int(
+                tick_in_usec
+                * TIME_UNITS_PER_SEC
+                * _tc_adjust_size((i + 1) << cell_log, mpu, linklayer)
+                / rate
+            )
+            for i in range(256)
+        ),
+    )
+    return (
+        tc_ratespec(
+            cell_log=cell_log,
+            linklayer=linklayer & TC_LINKLAYER_MASK,
+            overhead=overhead,
+            cell_align=-1,
+            mpu=mpu,
+            rate=MAX_U32 if rate > MAX_U32 else rate,
+        ),
+        rtab,
+    )
+
+
+def htb_class_attrs(
+    prio: int = 0,
+    mtu: int = 1600,
+    mpu: int = 0,
+    overhead: int = 0,
+    linklayer: int = TC_LINKLAYER_ETHERNET,
+    quantum: int = 0,
+    burst: Union[int, Tuple[int, int]] = 0,
+    cburst: Union[int, Tuple[int, int]] = 0,
+    ceil: int = 0,
+    rate: int = 0,
+) -> Tuple[Tuple[int, bytes], ...]:
+    assert rate, "Must specify non-zero rate"
+    if not ceil:
+        ceil = rate
+    buffer, cell_log = _get_size_and_cell(burst)
+    cbuffer, ccell_log = _get_size_and_cell(cburst)
+    if not buffer:
+        buffer = rate // HZ + mtu
+    if not cbuffer:
+        cbuffer = ceil // HZ + mtu
+    optrate, rtab = _make_ratespec_tab(
+        cell_log=cell_log,
+        linklayer=linklayer,
+        overhead=overhead,
+        mtu=mtu,
+        mpu=mpu,
+        rate=rate,
+    )
+    optceil, ctab = _make_ratespec_tab(
+        cell_log=ccell_log,
+        linklayer=linklayer,
+        overhead=overhead,
+        mtu=mtu,
+        mpu=mpu,
+        rate=ceil,
+    )
+    return (
+        (
+            TCA_OPTIONS,
+            (
+                pack_attr(
+                    TCA_HTB_PARMS,
+                    tc_htb_opt(
+                        rate=optrate.bytes,
+                        ceil=optceil.bytes,
+                        buffer=int(
+                            tick_in_usec * TIME_UNITS_PER_SEC * buffer / rate
+                        ),
+                        cbuffer=int(
+                            tick_in_usec * TIME_UNITS_PER_SEC * cbuffer / ceil
+                        ),
+                    ).bytes,
+                )
+                + (
+                    pack_attr(TCA_HTB_RATE64, pack("=L", rate))
+                    if rate > MAX_U32
+                    else b""
+                )
+                + (
+                    pack_attr(TCA_HTB_CEIL64, pack("=L", ceil))
+                    if ceil > MAX_U32
+                    else b""
+                )
+                + pack_attr(TCA_HTB_RTAB, rtab.tobytes())
+                + pack_attr(TCA_HTB_CTAB, ctab.tobytes())
+            ),
+        ),
+    )
+
+
 _extra_attrs: Dict[
     Tuple[int, str], Callable[..., Tuple[Tuple[int, bytes], ...]]
 ] = defaultdict(
@@ -173,6 +324,7 @@ _extra_attrs: Dict[
         (RTM_NEWQDISC, "pfifo"): fifo_qdisc_attrs,
         (RTM_NEWQDISC, "pfifo_head_drop"): fifo_qdisc_attrs,
         (RTM_NEWQDISC, "prio"): prio_qdisc_attrs,
+        (RTM_NEWTCLASS, "htb"): htb_class_attrs,
     },
 )
 
