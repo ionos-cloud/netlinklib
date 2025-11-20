816a06afbc9e0a85e1e3a1ebc393ae9e0b68d43e
Eugene Crosser <evgenii.cherkashin@ionos.com>
2025-11-20 14:55:42 +0100

    Remove legacy modules that rely on legacy classes
    Signed-off-by: Eugene Crosser <evgenii.cherkashin@ionos.com>

--- netlinklib/__init__.py
+++ netlinklib/__init__.py
@@ -1,10 +1,6 @@
 """Netlinklib: reexport all API objects"""
 
 # pylint: disable=wildcard-import, unused-wildcard-import
-from .api_link import *
-from .api_neigh import *
-from .api_route import *
-from .api_tc import *
 from .defs import *
 from .classes import *
 from .core import *
